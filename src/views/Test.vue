bd879db5b618556a891fd096cb3768912aa5774d
balao1312 <balao1014@hotmail.com>
2021-08-02 21:21:49 +0800

    use of grid

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="ctn">
-    <div
-      class="blk"
-      @mouseover="mouseEvent($event, 5)"
-      @mouseleave="mouseEvent($event, 8)"
-      @dblclick="mouseEvent"
-    >
-      Test Section
+  <div>
+    <!-- use of mouseEvent -->
+    <div class="ctn">
+      <div
+        class="blk"
+        @mouseover="mouseEvent($event, 5)"
+        @mouseleave="mouseEvent($event, 8)"
+        @dblclick="mouseEvent"
+      >
+        Test Section
+      </div>
+      <div class="blk" @mousemove="mouseMovement">
+        position - {{ x }} - {{ y }}
+      </div>
     </div>
-    <div class="blk" @mousemove="mouseMovement">
-      position - {{ x }} - {{ y }}
+    <br />
+
+    <!-- use of grid -->
+    <div class="grid">
+      <div class="grid-item">1</div>
+      <div class="grid-item">2</div>
+      <div class="grid-item">3</div>
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+      <div class="grid-item">6</div>
+      <div class="grid-item">7</div>
+      <div class="grid-item">8</div>
+      <div class="grid-item">9</div>
+      <div class="grid-item">10</div>
+      <div class="grid-item">11</div>
+      <div class="grid-item">12</div>
     </div>
   </div>
 </template>
@@ -53,5 +73,19 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-evenly;
+}
+.grid {
+  background-color: green;
+  display: grid;
+  padding: 10px;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  grid-template-columns: 200px 200px auto;
+}
+.grid-item {
+  background-color: white;
+  text-align: center;
+  font-size: 20px;
+  padding: 20px;
 }
 </style>
